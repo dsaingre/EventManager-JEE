3b3a2061f545c0ca976103242df9f42a67888202
Damien RAYMOND <dam.ray49@gmail.com>
2016-10-27 17:24:33 +0200

    [feature#12/auth] Use sql dsl in method findByEmailAndPassword

--- src/main/java/fr/lidadi/jee/eventmanager/app/person/PersonDao.java
+++ src/main/java/fr/lidadi/jee/eventmanager/app/person/PersonDao.java
@@ -7,30 +7,20 @@ import javax.persistence.criteria.*;
 import java.util.Optional;
 import java.util.UUID;
 
+import static fr.lidadi.jee.eventmanager.framework.dao.sqldsl.SQLRequestFactory.*;
+
 /**
  * Created by damien on 24/10/2016.
  */
 public class PersonDao extends Dao<Person, UUID> {
 
-
-
-
     public Optional<Person> findByEmailAndPassword(String email, String password){
-
-        // TODO hash pwd
-        CriteriaQuery<Person> query = criteriaBuilder.createQuery(Person.class);
-
-        Root<Person> from = query.from(Person.class);
-
-        query
-                .where(criteriaBuilder.and(
-                        criteriaBuilder.equal(from.get("email"), email),
-                        criteriaBuilder.equal(from.get("password"), password)
-                ));
-
-
-        TypedQuery<Person> query1 = em.createQuery(query);
-        return query1.getResultList().stream().findFirst();
+        return this.findBy(
+                where(and(
+                    equal("email", email),
+                    equal("password", password))
+                 )
+        ).stream().findFirst();
     }
 
 }
